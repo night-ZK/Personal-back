fa0056c1bc8d95e96a91c410dec96f69a5b0e9c0
localhost-Note <zxkwin@outlook.com>
2019-08-14 20:34:50 +0800

    personal-s init

---
+++ src/main/java/com/zxk/personals/PersonalSApplication.java
@@ -0,0 +1,13 @@
+package com.zxk.personals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalSApplication.class, args);
+    }
+
+}
